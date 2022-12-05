35543a601eb0db6da03a85a8f6c809642ba3841b
IOTrue <kjoonc.dev@gmail.com>
2022-12-05 20:24:09 +0900

    index

---
+++ js/main.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+
+//*회원가입
+//이메일 체크
+let inputEmail = document.querySelector('#input-email').value
+//비밀번호 체크
+let inputPW = document.querySelector('#input-pw').value
+//닉네임 체크
+let inputNickname = document.querySelector('#input-nickname').value
+//언어 체크
+let inputLang = document.querySelector('#input-lang').value
+//블로그 체크
+let inputBlog = document.querySelector('#input-blog').value
+// 체크
+let btnAdd = document.querySelector('#btnAdd')
+btnAdd.addEventListener('click',function(){
+    console.log(inputEmail)
+})
+
+//*로그인
+//이메일 체크
+//비밀번호 체크
+});
