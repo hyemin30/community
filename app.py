b96b0196b59046dbb094f32b1559c7a962df6d70
hyemin30 <116478121+hyemin30@users.noreply.github.com>
2022-12-06 14:56:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-<<<<<<< Updated upstream
-=======
+from importlib.resources import contents
+
 from flask import Flask, render_template, jsonify, request, make_response
 from pymongo import MongoClient
 
@@ -14,81 +14,107 @@ def home():
 #프로필 목록
 @app.route('/profile', methods=["GET"])
 def profile():
-    profile_list = list(db.users.find({}, {'_id': False}))
+    profile_list = list(db.members.find({}, {'_id': False}))
     return jsonify({'profile': profile_list})
 
 #게시글 목록
-@app.route('/postings', methods=["GET"])
+@app.route('/post', methods=["GET"])
 def postings():
     email = request.cookies.get("emailCookie")
-    posting_list = list(db.postings.find({}, {'_id': False}))
-    reading_list = list(db.read.find({'email': email}, {'_id': False}))
-    return jsonify({'postings': posting_list, 'readings': reading_list})
+    readings = list(db.read.find({'email': email}, {'_id': False}))
+    return jsonify({'readings': readings})
 
 #읽음
 @app.route('/read', methods=["POST"])
 def read():
-    num_receive = request.form['num_give']
+    num_receive = int(request.form['num_give'])
     email = request.cookies.get("emailCookie")
 
-    doc = {
-        'num': num_receive,
-        'email': email
-    }
-    db.read.insert_one(doc)
-
+    db.read.update_one({'email': email, 'num':num_receive}, {'$set': {'read': 1}})
     return jsonify({'msg': '읽기 완료'})
 
 #게시글 작성
-@app.route('/posting', methods=["POST"])
+@app.route('/post', methods=["POST"])
 def posting():
-    email = request.cookies.get("emailCookie")
     postings = list(db.postings.find({}, {'_id': False}))
     content_receive = request.form['content_give']
 
-    doc = {
-        'num' : len(postings)+1,
-        'content' : content_receive,
-        'email':email
-    }
-    db.content.insert_one(doc)
-    return jsonify({'msg': '작성 완료'})
-
-#게시글 수정화면
-@app.route('/edit', methods=["GET"])
-def edit_form():
-    num_receive = request.form['num_give']
-    posting = db.postings.find_one({'num': num_receive})
-    return jsonify({'posting': posting})
-
-#게시글 수정등록
-@app.route('/edit', methods=["POST"])
-def edit():
-    num_receive = request.form['num_give']
-    content_receive = request.form['post_give']
-    db.postings.update_one({'num': num_receive}, {'$set': {'content': content_receive}})
-    return jsonify({'msg': '작성 완료'})
-
-
-#좋아요
-@app.route('/like', methods=["POST"])
-def like():
-    num_receive = request.form['num_give']
-    email = request.cookies.get("emailCookie")
-    likes = list(db.likes.find({"num", num_receive}, {'_id': False}))
-    count = len(likes)
-
-    for like in likes:
-        if email == like['email']:
-            return jsonify({'msg': '이미 좋아요를 눌렀습니다'})
+    if content_receive == '' or content_receive is None:
+        return jsonify({'msg': '내용을 작성해주세요'})
 
     doc = {
-        'num': num_receive,
-        'email': email,
-        'count': count + 1
+        'num': len(postings)+1,
+        'content': content_receive,
     }
-    db.likes.insert_one(doc)
-    return jsonify({'msg': '좋아요 완료'})
+    db.postings.insert_one(doc)
+
+    members = list(db.members.find({}, {'_id': False}))
+
+    for member in members:
+        doc = {
+            'num': len(postings) + 1,
+            'content': content_receive,
+            'email': member['email'],
+            'read': 0
+        }
+        db.read.insert_one(doc)
+    return jsonify({'msg': '작성 완료'})
+#########################임시###############################
+# #게시글 수정화면
+# @app.route('/edit', methods=["GET"])
+# def edit_form():
+#     num_receive = request.form['num_give']
+#     posting = db.postings.find_one({'num': num_receive})
+#     return jsonify({'posting': posting})
+#
+# #게시글 수정등록
+# @app.route('/edit', methods=["POST"])
+# def edit():
+#     num_receive = request.form['num_give']
+#     content_receive = request.form['post_give']
+#
+#     if content_receive == '' or content_receive is None:
+#         return jsonify({'msg': '내용을 작성해주세요'})
+#
+#     db.postings.update_one({'num': num_receive}, {'$set': {'content': content_receive}})
+#     return jsonify({'msg': '작성 완료'})
+#
+#
+# #좋아요
+# @app.route('/like', methods=["POST"])
+# def like():
+#     # num_receive = int(request.form['num_give'])
+#     # email = request.cookies.get("emailCookie")
+#     num_receive = 3
+#     email='test2@naver.com'
+#     likes = list(db.likes.find({'num', num_receive}, {'_id': False}))
+#
+#     if len(likes) > 0:
+#         for like in likes:
+#             if email == like['email']:
+#                 return jsonify({'msg': '이미 좋아요를 눌렀습니다'})
+#         doc = {
+#             'num': num_receive,
+#             'email': email,
+#         }
+#         db.likes.insert_one(doc)
+#         return jsonify({'msg': '좋아요 완료'})
+#
+#     doc = {
+#         'num': num_receive,
+#         'email': email,
+#     }
+#     db.likes.insert_one(doc)
+#     return jsonify({'msg': '좋아요 완료'})
+#
+# #좋아요 개수 가져오기
+# @app.route('/like', methods=["GET"])
+# def get_like():
+#     likes = list(db.likes.find({}, {'_id': False}))
+#     print(len(likes))
+#     return jsonify({'likes': likes})
+#########################임시###############################
+
 
 #회원가입 페이지
 @app.route("/join", methods=["GET"])
@@ -124,6 +150,18 @@ def join_post():
     }
     db.members.insert_one(doc)
 
+    # 신규가입시 기존 게시글 read 테이블에 추가함
+    postings = list(db.postings.find({}, {'_id': False}))
+
+    for posting in postings:
+        doc = {
+            'num': posting['num'],
+            'content': posting['content'],
+            'email': email_receive,
+            'read': 0
+        }
+        db.read.insert_one(doc)
+
     return jsonify({'msg': '가입 완료'})
 
 
@@ -148,16 +186,5 @@ def login_post():
     else:
         return '비밀번호가 일치하지 않습니다.'
 
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-
-
-
-
-
-
-
-
->>>>>>> Stashed changes
